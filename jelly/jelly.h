15bfe0145051d125972b4cb9fd14299fb2a1c2f5
Kevin Loken <kevinloken@me.com>
2012-02-01 10:47:39 -0800

    Initial Commit

---
+++ jelly/jelly.h
@@ -0,0 +1,13 @@
+//
+//  jelly.h
+//  jelly
+//
+//  Created by Kevin Loken on 12-02-01.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jelly : NSObject
+
+@end
---
+++ jelly/jelly.m
@@ -0,0 +1,23 @@
+//
+//  jelly.m
+//  jelly
+//
+//  Created by Kevin Loken on 12-02-01.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "jelly.h"
+
+@implementation jelly
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
